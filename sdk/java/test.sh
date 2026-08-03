f648a7dd810e4b3fa95d8a01383ce5d10f9a7386
Alexander Mills <alex@oresoftware.com>
2026-08-02 23:23:24 -0500

    test(java): add strict standalone test runner

---
+++ sdk/java/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -eu
+rm -rf build
+mkdir -p build
+javac -Xlint:all -Werror -d build $(find src/main/java src/test/java -name '*.java' -print)
+java -ea -cp build com.oresoftware.nextloggers.NextLoggersTest
