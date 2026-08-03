50e40e2d3f04bf91767afe8a4645e7c0ed4e325a
Alexander Mills <alex@oresoftware.com>
2026-08-03 00:29:40 -0500

    test(erlang): run baseline and adversarial EUnit modules

--- sdk/erlang/test.sh
+++ sdk/erlang/test.sh
@@ -5,5 +5,8 @@ root=$(cd "$(dirname "$0")" && pwd)
 out="$root/.build"
 rm -rf "$out"
 mkdir -p "$out"
-erlc -Werror -o "$out" "$root/src/next_loggers.erl" "$root/test/next_loggers_tests.erl"
-erl -noshell -pa "$out" -eval 'case eunit:test(next_loggers_tests, [verbose]) of ok -> halt(0); _ -> halt(1) end.'
+erlc -Werror -o "$out" \
+  "$root/src/next_loggers.erl" \
+  "$root/test/next_loggers_tests.erl" \
+  "$root/test/next_loggers_adversarial_tests.erl"
+erl -noshell -pa "$out" -eval 'case eunit:test([next_loggers_tests, next_loggers_adversarial_tests], [verbose]) of ok -> halt(0); _ -> halt(1) end.'
