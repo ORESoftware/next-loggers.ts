0ab09e8f2701c258de09010174db17be3cb091e6
Alexander Mills <alex@oresoftware.com>
2026-08-03 00:24:40 -0500

    test(java): run baseline and adversarial suites

--- sdk/java/test.sh
+++ sdk/java/test.sh
@@ -9,3 +9,4 @@ mkdir -p "$out"
 find "$root/src/main/java" "$root/src/test/java" -name '*.java' -print0 \
   | xargs -0 javac --release 17 -Werror -Xlint:all -d "$out"
 java -ea -cp "$out" cloud.oresoftware.nextloggers.NextLoggersTest
+java -ea -cp "$out" cloud.oresoftware.nextloggers.NextLoggersAdversarialTest
