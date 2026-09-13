e47633d4d2870f14e440ebe2d8dcd1f989201c17
Alexander Mills <alex@oresoftware.com>
2026-09-12 22:40:55 -0500

    test(dart): run canonical standalone suites

--- sdk/dart/test.sh
+++ sdk/dart/test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 dart pub get
-dart analyze
-dart test
+dart format --output=none --set-exit-if-changed lib test
+dart analyze --fatal-infos lib test/conformance.dart test/context_shutdown.dart test/adversarial.dart test/next_loggers_test.dart
+
+dart run test/conformance.dart
+dart run test/context_shutdown.dart
+dart run test/adversarial.dart
+dart run test/next_loggers_test.dart
