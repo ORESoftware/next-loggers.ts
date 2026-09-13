6c5657827dd2abd6d22d2efb96e98fda6011aca5
Alexander Mills <alex@oresoftware.com>
2026-09-12 22:41:02 -0500

    test(dart): target canonical standalone test files

--- sdk/dart/test.sh
+++ sdk/dart/test.sh
@@ -2,9 +2,8 @@
 set -eu
 dart pub get
 dart format --output=none --set-exit-if-changed lib test
-dart analyze --fatal-infos lib test/conformance.dart test/context_shutdown.dart test/adversarial.dart test/next_loggers_test.dart
+dart analyze --fatal-infos lib test/conformance.dart test/context_shutdown.dart test/adversarial.dart
 
 dart run test/conformance.dart
 dart run test/context_shutdown.dart
 dart run test/adversarial.dart
-dart run test/next_loggers_test.dart
